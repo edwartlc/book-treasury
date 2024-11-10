eca1ce407fdf42d9f3a13973cdd617bc857526a1
Edwart Llanos <edwartlc@gmail.com>
2024-11-10 09:09:02 -0500

    First commit

---
+++ src/main/java/com/edwartlc/book_treasury/BookTreasuryApplication.java
@@ -0,0 +1,13 @@
+package com.edwartlc.book_treasury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTreasuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTreasuryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edwartlc/book_treasury/BookTreasuryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwartlc.book_treasury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTreasuryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
